8369e1011b925b8c758a748e43615c05f17d2312
GustaAzevedo <69991055+GustaAzevedo@users.noreply.github.com>
2021-03-17 11:08:36 -0300

    comecando a implementar tabuleiro

---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+
+        private Peca[,] Pecas;
+
+        public Tabuleiro(int linha, int coluna)
+        {
+            this.linhas = linha;
+            this.colunas = coluna;
+            Pecas = new Peca[linhas, colunas];
+        }
+    }
+}
