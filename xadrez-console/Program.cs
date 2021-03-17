07fd8e98d30dc126bf0bb5c5fefe69cdf4f53cd0
GustaAzevedo <69991055+GustaAzevedo@users.noreply.github.com>
2021-03-17 13:37:00 -0300

    modificando impressao do tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,18 +8,16 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez px = new PosicaoXadrez('c', 7);
-
-            Console.WriteLine(px);
-            Console.WriteLine(px.toPosicao());
-            /*
+          
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 5));
 
                 Tela.iprimirTabuleiro(tab);
             }
@@ -27,7 +25,7 @@ namespace xadrez_console
             {
                 Console.WriteLine(e.Message);
             }
-            */
+
            
         }
     }
