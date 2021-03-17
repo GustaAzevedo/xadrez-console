67b629cec2d5b8520c4902ec3e130672eefb0144
GustaAzevedo <69991055+GustaAzevedo@users.noreply.github.com>
2021-03-17 10:52:05 -0300

    Subindo primeira parte

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,15 @@
+﻿using tabuleiro;
+using System;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+        }
+    }
+}
